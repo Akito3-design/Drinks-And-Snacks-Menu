03f30658b1c0eca93244db69adf70824f9363f67
innocentiyy <lanshiftus@gmail.com>
2024-11-06 15:04:00 +0300

    Update TEST.py

--- TEST.py
+++ TEST.py
@@ -40,37 +40,51 @@
 import openpyxl
 
 def просмотреть_файл():
- """Просматривает содержимое файла Excel."""
+    """Просматривает содержимое файла Excel и позволяет выбрать лист, а также возвращаться к выбору листа."""
+    
+    файл = input("Введите имя файла Excel: ")
+    try:
+        wb = openpyxl.load_workbook(файл + ".xlsx")  # Добавляем расширение .xlsx
 
- файл = input("Введите имя файла Excel: ")
- try:
-  wb = openpyxl.load_workbook(файл + ".xlsx") # Добавляем расширение .xlsx
-  sheet = wb.active # Выбираем активный лист
+        while True:
+            print("\nДоступные листы:")
+            for index, sheet in enumerate(wb.sheetnames):
+                print(f"{index + 1}. {sheet}")
+            print("0. Назад")
 
-  print("Лист:", sheet.title)
-  for row in sheet.iter_rows(values_only=True):
-   print(row)
+            лист_выбор = int(input("\nВведите номер листа для просмотра или 0 для возврата: ")) - 1
 
- except FileNotFoundError:
-  print("Файл не найден.")
+            if лист_выбор == -1:
+                break  # Возвращаемся в главное меню
+            elif 0 <= лист_выбор < len(wb.sheetnames):
+                sheet = wb.worksheets[лист_выбор]  # Выбираем лист по индексу
+                print("\nЛист:", sheet.title)
+                for row in sheet.iter_rows(values_only=True):
+                    print(row)
+            else:
+                print("Неверный номер листа.")
+                
+    except FileNotFoundError:
+        print("Файл не найден.")
 
 def главное_меню():
- """Отображает главное меню."""
-
- print("\nГлавное меню:")
- print("1. Просмотреть файл Excel")
- print("2. Выход")
-
- выбор = input("Выберите пункт меню: ")
- return выбор
+    """Отображает главное меню."""
+    
+    print("\nГлавное меню:")
+    print("1. Просмотреть файл Excel")
+    print("2. Выход")
+    
+    выбор = input("Выберите пункт меню: ")
+    return выбор
 
 if __name__ == "__main__":
- while True:
-  выбор = главное_меню()
-  if выбор == '1':
-   просмотреть_файл()
-  elif выбор == '2':
-   break
-  else:
-   print("Неверный выбор.")
+    while True:
+        выбор = главное_меню()
+        if выбор == '1':
+            просмотреть_файл()
+        elif выбор == '2':
+            break
+        else:
+            print("Неверный выбор.")
+
 
