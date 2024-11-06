bcdafd05ffccc6b25b562dbf419b132b9e78e3b7
Akito3-design <egorkaevtk@gmail.com>
2024-11-06 14:56:29 +0300

    Update 123.py
    Появилась кнопка "Назад"

--- 123.py
+++ 123.py
@@ -1,26 +1,30 @@
 import openpyxl
 
 def просмотреть_файл():
-    """Просматривает содержимое файла Excel и позволяет выбрать лист."""
+    """Просматривает содержимое файла Excel и позволяет выбрать лист, а также возвращаться к выбору листа."""
     
     файл = input("Введите имя файла Excel: ")
     try:
         wb = openpyxl.load_workbook(файл + ".xlsx")  # Добавляем расширение .xlsx
-        
-        print("\nДоступные листы:")
-        for index, sheet in enumerate(wb.sheetnames):
-            print(f"{index + 1}. {sheet}")
-        
-        лист_выбор = int(input("\nВведите номер листа для просмотра: ")) - 1
-        
-        if 0 <= лист_выбор < len(wb.sheetnames):
-            sheet = wb.worksheets[лист_выбор]  # Выбираем лист по индексу
-            print("\nЛист:", sheet.title)
-            for row in sheet.iter_rows(values_only=True):
-                print(row)
-        else:
-            print("Неверный номер листа.")
-            
+
+        while True:
+            print("\nДоступные листы:")
+            for index, sheet in enumerate(wb.sheetnames):
+                print(f"{index + 1}. {sheet}")
+            print("0. Назад")
+
+            лист_выбор = int(input("\nВведите номер листа для просмотра или 0 для возврата: ")) - 1
+
+            if лист_выбор == -1:
+                break  # Возвращаемся в главное меню
+            elif 0 <= лист_выбор < len(wb.sheetnames):
+                sheet = wb.worksheets[лист_выбор]  # Выбираем лист по индексу
+                print("\nЛист:", sheet.title)
+                for row in sheet.iter_rows(values_only=True):
+                    print(row)
+            else:
+                print("Неверный номер листа.")
+                
     except FileNotFoundError:
         print("Файл не найден.")
 
@@ -34,7 +38,7 @@ def главное_меню():
     выбор = input("Выберите пункт меню: ")
     return выбор
 
-if __name__ == "__main__":
+if name == "main":
     while True:
         выбор = главное_меню()
         if выбор == '1':
